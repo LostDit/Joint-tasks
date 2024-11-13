2a7b7b4e2d51aa305d26d9b667c5d7879bbc17e5
Maksim <maksim.antonov2012@yandex.ru>
2024-11-13 19:09:43 +0300

    Проверка на ограничение вводимого массива
    Можно сделать более проще, ну там сами можете посмотреть как это можно сделать проще откиньте коммиты на строчки, дополню по этим коммитам

--- LeetCode/One plus/One plus.cpp
+++ LeetCode/One plus/One plus.cpp
@@ -1,20 +1,35 @@
-﻿// One plus.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <string>
+std::string proverkavvoda(std::string digits) { //функция на проверку ограничений по заданию Constraints: 
+//1.(1 <= digits.length <= 100) 
+//2.(0 <= digits[i] <= 9) 
+//3.(digits does not contain any leading 0's.)
+    bool chek = true, final = true;
+    while (final) {
+        system("cls");
+        chek == true ? std::cout << "Enter the value of the digits: " : std::cout << "Error try to enter the value of the digits again: ";
+        getline(std::cin, digits);
+        digits.length() <= 100 || digits.length() >= 1 ? chek = true : chek = false;
+        for (int i = 0; i < digits.length() && chek == true; ++i) {
+            if (digits[0] == '0') {
+                chek = false;
+            }
+            if (digits[i] >= '0' && digits[i] <= '9') {
+                continue;
+            }
+            else {
+                chek = false;
+            }
+        }
+        if (chek == true) {
+            final = false;
+        }
+    }
+    return digits;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main() {
+    std::string digits;
+    digits = proverkavvoda(digits);
+    std::cout << digits;
+}
