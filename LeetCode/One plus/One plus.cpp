1cbac94b96357b3dec7ce0dd9ca8c5c9997a16c2
Maksim <maksim.antonov2012@yandex.ru>
2024-11-15 03:19:34 +0300

    Какаха
    Ещё какая

--- LeetCode/One plus/One plus.cpp
+++ LeetCode/One plus/One plus.cpp
@@ -1,35 +1,13 @@
-#include <iostream>
-#include <string>
-std::string proverkavvoda(std::string digits) { //функция на проверку ограничений по заданию Constraints: 
-//1.(1 <= digits.length <= 100) 
-//2.(0 <= digits[i] <= 9) 
-//3.(digits does not contain any leading 0's.)
-    bool chek = true, final = true;
-    while (final) {
-        system("cls");
-        chek == true ? std::cout << "Enter the value of the digits: " : std::cout << "Error try to enter the value of the digits again: ";
-        getline(std::cin, digits);
-        digits.length() <= 100 || digits.length() >= 1 ? chek = true : chek = false;
-        for (int i = 0; i < digits.length() && chek == true; ++i) {
-            if (digits[0] == '0') {
-                chek = false;
-            }
-            if (digits[i] >= '0' && digits[i] <= '9') {
-                continue;
-            }
-            else {
-                chek = false;
-            }
+#include <vector>
+class Solution { //весь код прогоняется внутри сайта через дебаггер, не знал поэтому изначально решил какую-то фигню делать
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            ++digits[i];
+            digits[i] %= 10;
+            if (digits[i] != 0) return digits;
         }
-        if (chek == true) {
-            final = false;
-        }
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
-    return digits;
-}
-
-int main() {
-    std::string digits;
-    digits = proverkavvoda(digits);
-    std::cout << digits;
-}
+};//по факту разобрался как работать с массивом
